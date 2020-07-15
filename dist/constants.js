66be013a8e0873ef8f0a178d0a5882141ec8bcbb
Fernando Asulay <fer.guitar.ln@gmail.com>
2020-07-15 09:12:11 +0200

    delete dist

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routerMeta = exports.endpointMeta = exports.paramTypes = void 0;
-exports.paramTypes = 'design:paramtypes';
-exports.endpointMeta = 'custom:endpoint';
-exports.routerMeta = 'custom:router';
--- dist/service-decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Service = void 0;
-var di_container_1 = require("./di-container");
-exports.Service = function (options) {
-    return function (target) {
-        di_container_1.container.register(target, options);
-    };
-};
